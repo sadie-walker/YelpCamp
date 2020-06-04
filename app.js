05d6c9d4007c1d96275f354023b0e5fef0f2c6ba
sadie-walker <sadie-walker@users.noreply.github.com>
2020-06-04 18:08:49 +0100

    spelling error correction ln5

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000; 
 
-app.get("view engine", "ejs");
+app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
     res.render("landing");
