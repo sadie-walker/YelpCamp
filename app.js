83428bd737199a35d2ae257d0b42c1dfeb3f4d6a
sadie-walker <sadie-walker@users.noreply.github.com>
2020-06-04 18:04:01 +0100

    add landing page/intial route

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const port = process.env.PORT || 3000;
 
 app.get("view engine", "ejs");
 
+app.get("/", function(req, res){
+    res.render("landinge");
+})
+
 app.listen(port, function(){
     console.log("server is running");
 })
