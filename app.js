03dff3cdf0d663de145d275c2d1db190e3a45a14
sadie-walker <sadie-walker@users.noreply.github.com>
2020-06-04 18:20:51 +0100

    add campground array and pass into /campgrounds route render

--- app.js
+++ app.js
@@ -9,7 +9,13 @@ app.get("/", function(req, res){
 })
 
 app.get("/campgrounds", function(req,res){
-    res.render("campgrounds");
+    const campgrounds = [
+        {name: "Camp1", image: "https://images.unsplash.com/photo-1510312305653-8ed496efae75?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
+        {name: "Camp2", image: "https://images.unsplash.com/photo-1537565266759-34bbc16be345?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
+        {name: "Camp3", image: "https://images.unsplash.com/photo-1563299796-17596ed6b017?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
+        {name: "Camp4", image: "https://images.unsplash.com/photo-1508873696983-2dfd5898f08b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
+    ]
+    res.render("campgrounds",{campgrounds: campgrounds});
 })
 
 app.listen(port, function(){
