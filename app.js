7e2164b73733c70dd56713d628fce4dc620562ae
sadie-walker <sadie-walker@users.noreply.github.com>
2020-06-05 11:22:28 +0100

    push new campground to campgrounds array

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000; 
 
+const campgrounds = [
+    {name: "Camp1", image: "https://images.unsplash.com/photo-1510312305653-8ed496efae75?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
+    {name: "Camp2", image: "https://images.unsplash.com/photo-1537565266759-34bbc16be345?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
+    {name: "Camp3", image: "https://images.unsplash.com/photo-1563299796-17596ed6b017?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
+    {name: "Camp4", image: "https://images.unsplash.com/photo-1508873696983-2dfd5898f08b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
+]
+
 app.use(express.json());
 app.use(express.urlencoded());
 app.set("view engine", "ejs");
@@ -11,17 +18,15 @@ app.get("/", function(req, res){
 })
 
 app.get("/campgrounds", function(req,res){
-    const campgrounds = [
-        {name: "Camp1", image: "https://images.unsplash.com/photo-1510312305653-8ed496efae75?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
-        {name: "Camp2", image: "https://images.unsplash.com/photo-1537565266759-34bbc16be345?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
-        {name: "Camp3", image: "https://images.unsplash.com/photo-1563299796-17596ed6b017?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
-        {name: "Camp4", image: "https://images.unsplash.com/photo-1508873696983-2dfd5898f08b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
-    ]
     res.render("campgrounds",{campgrounds: campgrounds});
 })
 
 app.post("/campgrounds", function(req,res){
+    let campInput = req.body.newCampInput;
+    let imageInput = req.body.newCampImageInput
 
+    campgrounds.push({name: campInput, image: imageInput});
+    res.redirect("/campgrounds");
 })
 
 app.get("/campgrounds/new", function(req,res){
