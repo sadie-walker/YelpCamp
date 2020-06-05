b3d39430cc2f553f7ff6947039adaddd2001594b
sadie-walker <sadie-walker@users.noreply.github.com>
2020-06-05 10:57:43 +0100

    add new-camp.ejs and new campground  route

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000; 
 
+app.use(express.json());
+app.use(express.urlencoded());
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
@@ -20,6 +22,10 @@ app.get("/campgrounds", function(req,res){
 
 app.post("/campgrounds", function(req,res){
 
+})
+
+app.get("/campgrounds/new", function(req,res){
+    res.render("new-camp");
 })
 
 app.listen(port, function(){
