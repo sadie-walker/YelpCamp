cf8b2b4ce124b9f9ac005847534f5508673a4b57
sadie-walker <sadie-walker@users.noreply.github.com>
2020-06-26 15:46:12 +0100

    add rating to the comment model

--- models/comment.js
+++ models/comment.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const commentSchema = new mongoose.Schema({
     text: String,
+    rating: Number,
     author: {
         id: {
             type: mongoose.Schema.Types.ObjectId,
